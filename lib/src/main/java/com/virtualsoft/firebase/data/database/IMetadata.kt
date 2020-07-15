437054e16bd6d82267aa2c6cc350708c22937f3e
Abner Lima <abner.lima27@outlook.com>
2020-07-15 01:08:27 -0300

    update firestore operations

---
+++ lib/src/main/java/com/virtualsoft/firebase/data/database/IMetadata.kt
@@ -0,0 +1,5 @@
+package com.virtualsoft.firebase.data.database
+
+import com.virtualsoft.core.service.database.data.IDocument
+
+interface IMetadata : IDocument
--- lib/src/main/java/com/virtualsoft/firebase/services/firestore/IFirestore.kt
+++ lib/src/main/java/com/virtualsoft/firebase/services/firestore/IFirestore.kt
@@ -5,27 +5,14 @@ import com.virtualsoft.core.service.database.ITreeDatabase
 import com.virtualsoft.core.utils.AppUtils.getEnvironment
 import com.virtualsoft.firebase.IFirebase
 
-interface IFirestore : IFirebase, ITreeDatabase {
+interface IFirestore : IFirebase {
 
     var context: Context?
 
     companion object {
 
-        //COLLECTIONS
         fun metadataCollection(): String {
             return "metadata"
-        }
-
-        fun treedataCollection(): String {
-            return "treedata"
-        }
-
-        fun getParentPath(documentPath: String): String {
-            return documentPath.split("/").dropLast(1).joinToString("/")
-        }
-
-        fun getChildsPath(documentPath: String): String {
-            return "$documentPath/${treedataCollection()}"
         }
     }
 }
